277e9f6ad7e9daa66f5ec0362468a2510ed979a7
chithrapoonacha <41833719+chithrapoonacha@users.noreply.github.com>
2020-07-19 13:27:09 -0700

    Add files via upload

---
+++ Python Notes.py
@@ -0,0 +1,54 @@
+Python Notes 
+
+#Data types :  int, float , complex, str , bool, list, tuple , set, dict
+
+print(2+5)          #performs operations
+#7       
+
+print(type(2+4))   #displays the datatype of the output like int float
+<int>  
+print(type(2-4))
+print(type(2*4))
+print(type(2/4))
+
+print(2**2)   #power of 
+print(2//5)   #round the value to int
+print(2 % 5)  #reminder of the division
+
+
+#Math functions
+
+print(round(3.9))  #rounds the value
+print(abs(-20))    #absolute value
+
+
+#Operator precedence
+
+print(20 - 3*4)
+
+print((5 + 4) * 10 / 2)
+#45
+
+print(((5 + 4) * 10) / 2)
+#45
+
+print((5 + 4) * (10 / 2))
+#45
+
+print(5 + (4 * 10) / 2)
+#25
+
+print(5 + 4 * 10 // 2)
+#25
+
+
+#binary and complex #complex is a datatype for complex numbers #binary is 1s and 0s
+
+print(bin(5))  #converts int to binary
+#0b101
+
+print(int('0b101', 2))  #converts binary to int
+#5
+
+
+
